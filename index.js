22b21d896811dda062e903e845029ff29360c644
Manny <mhfernan35277@gmail.com>
2023-06-23 19:12:11 -0700

    Adds myfile.md

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n = n + 1;
+    return n; 
+}
+function decrement(n) {
+    n = n - 1;
+    return n;
+}
+function makeInt(n) {
+    parseInt("n", 10);
+
+    return parseFloat(n);
+}
+   
+    function preserveDecimal(n) {
+    parseFloat('n');
+    return n * 1;
+}
